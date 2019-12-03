c4b4d0e8d87a1945eb82845790d35c5aac139873
dalilas98 <dalilascott98@gmail.com>
2019-12-03 00:20:51 -0500

    header color attempt1

--- src/containers/home.js
+++ src/containers/home.js
@@ -15,6 +15,7 @@ class LoginSignup extends Component {
                     <center>
                         <div>
                             <h1>Welcome to the Pre-Game!</h1>
+                            <h1 style="color: rgb(255, 0, 200; font-size:16px">Big Heading</h1>
                             <Link to="/login"><RaisedButton type="submit" primary={false}>Log In</RaisedButton></Link>
                             <Link to="/signup"><RaisedButton type="submit" primary={true} style={{ margin: 12 }}>Sign Up</RaisedButton></Link>
                         </div>
